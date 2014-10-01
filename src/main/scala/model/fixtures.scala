2619d51af19504bdd672dd9e2aca6d423c1e0f8e
Konstantin Laufer <laufer@cs.luc.edu>
2014-09-30 19:21:30 -0500

    removed extraneous uses of new in fixtures

--- src/main/scala/model/fixtures.scala
+++ src/main/scala/model/fixtures.scala
@@ -8,47 +8,47 @@ object fixtures {
 
   val simpleRectangle = Rectangle(80, 120)
 
-  val simplePolygon = new Polygon(
-    new Point(50, 50),
-    new Point(60, 100),
-    new Point(100, 110),
-    new Point(120, 60)
+  val simplePolygon = Polygon(
+    Point(50, 50),
+    Point(60, 100),
+    Point(100, 110),
+    Point(120, 60)
   )
 
-  val simpleLocation = new Location(70, 30, new Rectangle(80, 120))
+  val simpleLocation = Location(70, 30, Rectangle(80, 120))
 
-  val simpleFill = new Fill(new Rectangle(80, 120))
+  val simpleFill = Fill(Rectangle(80, 120))
 
-  val simpleOutline = new Outline(new Rectangle(80, 120));
+  val simpleOutline = Outline(Rectangle(80, 120));
 
-  val simpleStroke = new Stroke(Color.RED, new Rectangle(80, 120));
+  val simpleStroke = Stroke(Color.RED, Rectangle(80, 120));
 
-  val simpleGroup = new Group(
-    new Location(200, 100, new Circle(50)),
-    new Location(400, 300, new Rectangle(100, 50))
+  val simpleGroup = Group(
+    Location(200, 100, Circle(50)),
+    Location(400, 300, Rectangle(100, 50))
   )
 
-  val complexGroup = new Location(50, 100,
-    new Group(
-      new Circle(20),
-      new Rectangle(100, 200),
-      new Location(150, 50,
-        new Stroke(Color.RED,
-          new Fill(
-            new Group(
-              new Rectangle(50, 30),
-              new Outline(new Rectangle(300, 60)),
-              new Stroke(Color.CYAN,
-                new Polygon(
-                  new Point(50, 50),
-                  new Point(60, 100),
-                  new Point(100, 110),
-                  new Point(120, 60)
+  val complexGroup = Location(50, 100,
+    Group(
+      Circle(20),
+      Rectangle(100, 200),
+      Location(150, 50,
+        Stroke(Color.RED,
+          Fill(
+            Group(
+              Rectangle(50, 30),
+              Outline(Rectangle(300, 60)),
+              Stroke(Color.CYAN,
+                Polygon(
+                  Point(50, 50),
+                  Point(60, 100),
+                  Point(100, 110),
+                  Point(120, 60)
                 )
               ),
-              new Location(100, 200,
-                new Stroke(Color.MAGENTA,
-                  new Outline(new Circle(50))
+              Location(100, 200,
+                Stroke(Color.MAGENTA,
+                  Outline(Circle(50))
                 )
               )
             )
